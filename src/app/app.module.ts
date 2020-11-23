33f91403b4d8809615d0666e7c2c4b71e6323c25
gabrypol <gabrielepolenta@gmail.com>
2020-11-23 20:30:22 +0100

    Implemented lazy loading on the following modules: - recipes -shopping-list -auth Implemented preloading strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 
 @NgModule({
@@ -20,9 +17,6 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    AuthModule,
-    RecipesModule,
-    ShoppingListModule,
     SharedModule,
     CoreModule
   ],
